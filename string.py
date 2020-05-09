f29c9ea03dd468afb9c02a58f0b54775c0f64767
simon <simonzhang0428@gmail.com>
2020-05-08 22:45:12 -0700

    Update string.py

--- string.py
+++ string.py
@@ -151,5 +151,55 @@ def is_palinfrome(word):
         return False
     return is_palinfrome(middle(word))
 
-print(is_palinfrome('allen'))
-print(is_palinfrome('bob'))
+def is_palinfrome2(word):
+    return word == word[::-1]
+
+print(is_palinfrome2('allen'))
+print(is_palinfrome2('bob'))
+
+
+# A Caesar cypher is a weak form of encryption that involves “rotating” each letter
+# by a fixed number of places.
+# To rotate a letter means to shift it through the alphabet, 
+# wrapping around to the beginning if necessary,
+# so ‘A’ rotated by 3 is ‘D’ and ‘Z’ rotated by 1 is ‘A’.
+
+
+def rotate_letter(letter, n):
+    """Rotates a letter by n places.  Does not change other chars.
+
+    letter: single-letter string
+    n: int
+
+    Returns: single-letter string
+    """
+    if letter.isupper():
+        start = ord('A')
+    elif letter.islower():
+        start = ord('a')
+    else:
+        return letter
+
+    c = ord(letter) - start
+    i = (c + n) % 26 + start
+    return chr(i)
+
+
+def rotate_word(word, n):
+    """Rotates a word by n places.
+
+    word: string
+    n: integer
+
+    Returns: string
+    """
+    res = ''
+    for letter in word:
+        res += rotate_letter(letter, n)
+    return res
+
+
+if __name__ == '__main__':
+    print(rotate_word('cheer', 7))
+    print(rotate_word('melon', -10))
+    print(rotate_word('sleep', 9))
